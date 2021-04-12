7217b07f7fc2a738e001a47c0f5f474520647afa
RubySrivastava <ruby.srivas1984@gmail.com>
2021-04-12 08:40:37 +0000

    Create method edit_category

--- app.py
+++ app.py
@@ -221,23 +221,38 @@ def get_categories():
     return render_template("categories.html", categories=categories)
 
 
-#--Add New Category To DB--#
 @app.route("/add_category", methods=["GET", "POST"])
 def add_category():
-    if not session.get("user"):
-        return render_template("404.html")
-
     if request.method == "POST":
         category = {
-            "category_name": request.form.get("category_name"),
+            "category_name": request.form.get("category_name")
         }
         mongo.db.categories.insert_one(category)
-        flash("Category is successfully added")
-        return redirect(url_for("mypage", username=session['user']))
+        flash("New Category Added")
+        return redirect(url_for("get_categories"))
 
-    categories = mongo.db.categories.find().sort("category_name", 1)
-    return render_template(
-        "mypage.html", categories=categories)
+    return render_template("add_category.html")
+
+
+@app.route("/edit_category/<category_id>", methods=["GET", "POST"])
+def edit_category(category_id):
+    if request.method == "POST":
+        submit = {
+            "category_name": request.form.get("category_name")
+        }
+        mongo.db.categories.update({"_id": ObjectId(category_id)}, submit)
+        flash("Category Successfully Updated")
+        return redirect(url_for("get_categories"))
+
+    category = mongo.db.categories.find_one({"_id": ObjectId(category_id)})
+    return render_template("edit_category.html", category=category)
+
+
+@app.route("/delete_category/<category_id>")
+def delete_category(category_id):
+    mongo.db.categories.remove({"_id": ObjectId(category_id)})
+    flash("Category Successfully Deleted")
+    return redirect(url_for("get_categories"))
 
 
 #--Subscribe Newsletter--#
